0a631dba0125d3761e919dc8e8df7aa9ea75ee5e
donna <donnaxnguyen@yahoo.com>
2020-07-10 16:06:16 -0700

    commiting npm stuff

---
+++ index.js
@@ -0,0 +1,82 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the Title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "githubName",
+    },
+    {
+        type: "input",
+        message: "Provide a brief description of the project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Type of license?",
+        name: "badge",
+        choices: ["MIT", "ISC", "GPL", "BSD"],
+    },
+    {
+        type: "input",
+        message: "If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.",
+        name: "contributors",
+    },
+    {
+        type: "input",
+        message: "If you have writtend tests for your application, provide examples on how to run them.",
+        name: "tests",
+    },
+
+
+];
+function getUserInputs() {
+    inquirer.prompt(questions)
+        .then(function (response) {
+            console.log(response);
+            var readmeText = `
+# Project title: ${response.title} 
+## Project description
+${response.description} 
+## Table of Contents
+* [Installation](##installation)
+* [Usage](##Usage)
+* [License](##License)
+* [Contributing](##Contributing)
+* [Tests](##Tests)
+* [Questions](##Questions)
+### Installation
+${response.installation}
+### Usage
+${response.usage}
+### Contributing
+${response.contributors}
+### Tests
+${response.usage}
+### Questions
+### GitHub: ${response.githubName}  
+### License
+This project is licensed under the ![GitHub license](https://img.shields.io/badge/license-${response.badge}-blue.svg).
+        `
+            fs.writeFileSync("newREADME.md", readmeText, function () {
+                console.log("readme generated")
+            })
+        })
+}
+getUserInputs();
---
+++ newREADME.md
@@ -0,0 +1,24 @@
+
+# Project title: Readme Generator! 
+## Project description
+A project that creates a command-land application and allows a user's input to generate into a readme.md 
+## Table of Contents
+* [Installation](##installation)
+* [Usage](##Usage)
+* [License](##License)
+* [Contributing](##Contributing)
+* [Tests](##Tests)
+* [Questions](##Questions)
+### Installation
+clone the github link and run node index.js in the terminal
+### Usage
+see screenshots!
+### Contributing
+
+### Tests
+see screenshots!
+### Questions
+### GitHub: donnaxnguyen  
+### License
+This project is licensed under the ![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg).
+        
